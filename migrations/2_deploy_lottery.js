d38fc3e77ac11e8a81c12eaac413bbafc6a92cfb
Sam <7070683+santiq@users.noreply.github.com>
2021-05-22 19:24:15 -0300

    Add deployment script for erc20 token and market

--- migrations/2_deploy_lottery.js
+++ migrations/2_deploy_lottery.js
@@ -1,6 +1,19 @@
-const Lottery = artifacts.require("Lottery");
+const QuiniToken = artifacts.require("QuiniToken");
+const QuiniMarket = artifacts.require("QuiniMarket");
 
-module.exports = function (deployer) {
-  const initialTicketValue = 250000000000000000;
-  deployer.deploy(Lottery, initialTicketValue);
+module.exports = async function (deployer, network, accounts) {
+
+  const initialSupply = '21000000'
+
+  await deployer.deploy(QuiniToken, initialSupply);
+  // console.log('Quini Token Address: ', quiniToken.address);
+
+  await deployer.deploy(QuiniMarket, QuiniToken.address);
+
+  // console.log('Quini Market Address: ', quiniMarket.address);
+
+  const tokenInstance = await QuiniToken.deployed();
+
+  await tokenInstance.approve(QuiniMarket.address, initialSupply);
+
 };
